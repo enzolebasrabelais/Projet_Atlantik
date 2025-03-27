db4917c0bd3b3cc19d2bb4392f529bda87e5da32
enzolebasrabelais <enzolebas190106@gmail.com>
2025-03-27 15:26:56 +0100

    Ajouter Traversee 1

---
+++ Projet_Atlantik/FormAjouterTraversee.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Projet_Atlantik
+{
+    partial class FormAjouterTraversee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpDepart = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dtpDepart
+            // 
+            this.dtpDepart.CustomFormat = "dd/MM/yy  HH:mm";
+            this.dtpDepart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDepart.Location = new System.Drawing.Point(490, 240);
+            this.dtpDepart.Name = "dtpDepart";
+            this.dtpDepart.Size = new System.Drawing.Size(200, 20);
+            this.dtpDepart.TabIndex = 0;
+            // 
+            // FormAjouterTraversee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtpDepart);
+            this.Name = "FormAjouterTraversee";
+            this.Text = "Ajouter une traversée";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDepart;
+    }
+}
