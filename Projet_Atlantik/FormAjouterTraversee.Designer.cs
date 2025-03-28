1974d8e37841e6d4a49eaf6e47b90e89e15a134f
enzolebasrabelais <enzolebas190106@gmail.com>
2025-03-28 16:18:58 +0100

    Ajouter Traversee 2

--- Projet_Atlantik/FormAjouterTraversee.Designer.cs
+++ Projet_Atlantik/FormAjouterTraversee.Designer.cs
@@ -29,31 +29,152 @@
         private void InitializeComponent()
         {
             this.dtpDepart = new System.Windows.Forms.DateTimePicker();
+            this.dtpArrivee = new System.Windows.Forms.DateTimePicker();
+            this.lbxSecteursTraversee = new System.Windows.Forms.ListBox();
+            this.cmbLiaisonsTraversee = new System.Windows.Forms.ComboBox();
+            this.cmbBateauTraversee = new System.Windows.Forms.ComboBox();
+            this.btnAjouterTraversee = new System.Windows.Forms.Button();
+            this.lblSecteursTraversee = new System.Windows.Forms.Label();
+            this.lblLiaisonTraversee = new System.Windows.Forms.Label();
+            this.lblBateauTraversee = new System.Windows.Forms.Label();
+            this.lblDateDepart = new System.Windows.Forms.Label();
+            this.lblDateArrivee = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dtpDepart
             // 
-            this.dtpDepart.CustomFormat = "dd/MM/yy  HH:mm";
+            this.dtpDepart.CustomFormat = "yyyy/MM/dd  HH:mm:00";
             this.dtpDepart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtpDepart.Location = new System.Drawing.Point(490, 240);
             this.dtpDepart.Name = "dtpDepart";
             this.dtpDepart.Size = new System.Drawing.Size(200, 20);
             this.dtpDepart.TabIndex = 0;
             // 
+            // dtpArrivee
+            // 
+            this.dtpArrivee.CustomFormat = "yyyy/MM/dd  HH:mm:00";
+            this.dtpArrivee.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpArrivee.Location = new System.Drawing.Point(490, 286);
+            this.dtpArrivee.Name = "dtpArrivee";
+            this.dtpArrivee.Size = new System.Drawing.Size(200, 20);
+            this.dtpArrivee.TabIndex = 1;
+            // 
+            // lbxSecteursTraversee
+            // 
+            this.lbxSecteursTraversee.FormattingEnabled = true;
+            this.lbxSecteursTraversee.Location = new System.Drawing.Point(70, 71);
+            this.lbxSecteursTraversee.Name = "lbxSecteursTraversee";
+            this.lbxSecteursTraversee.Size = new System.Drawing.Size(120, 95);
+            this.lbxSecteursTraversee.TabIndex = 2;
+            this.lbxSecteursTraversee.SelectedIndexChanged += new System.EventHandler(this.lbxSecteursTraversee_SelectedIndexChanged);
+            // 
+            // cmbLiaisonsTraversee
+            // 
+            this.cmbLiaisonsTraversee.FormattingEnabled = true;
+            this.cmbLiaisonsTraversee.Location = new System.Drawing.Point(70, 325);
+            this.cmbLiaisonsTraversee.Name = "cmbLiaisonsTraversee";
+            this.cmbLiaisonsTraversee.Size = new System.Drawing.Size(121, 21);
+            this.cmbLiaisonsTraversee.TabIndex = 3;
+            // 
+            // cmbBateauTraversee
+            // 
+            this.cmbBateauTraversee.FormattingEnabled = true;
+            this.cmbBateauTraversee.Location = new System.Drawing.Point(490, 46);
+            this.cmbBateauTraversee.Name = "cmbBateauTraversee";
+            this.cmbBateauTraversee.Size = new System.Drawing.Size(121, 21);
+            this.cmbBateauTraversee.TabIndex = 4;
+            // 
+            // btnAjouterTraversee
+            // 
+            this.btnAjouterTraversee.Location = new System.Drawing.Point(461, 343);
+            this.btnAjouterTraversee.Name = "btnAjouterTraversee";
+            this.btnAjouterTraversee.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouterTraversee.TabIndex = 5;
+            this.btnAjouterTraversee.Text = "Ajouter";
+            this.btnAjouterTraversee.UseVisualStyleBackColor = true;
+            this.btnAjouterTraversee.Click += new System.EventHandler(this.btnAjouterTraversee_Click);
+            // 
+            // lblSecteursTraversee
+            // 
+            this.lblSecteursTraversee.AutoSize = true;
+            this.lblSecteursTraversee.Location = new System.Drawing.Point(70, 31);
+            this.lblSecteursTraversee.Name = "lblSecteursTraversee";
+            this.lblSecteursTraversee.Size = new System.Drawing.Size(55, 13);
+            this.lblSecteursTraversee.TabIndex = 6;
+            this.lblSecteursTraversee.Text = "Secteurs :";
+            // 
+            // lblLiaisonTraversee
+            // 
+            this.lblLiaisonTraversee.AutoSize = true;
+            this.lblLiaisonTraversee.Location = new System.Drawing.Point(73, 286);
+            this.lblLiaisonTraversee.Name = "lblLiaisonTraversee";
+            this.lblLiaisonTraversee.Size = new System.Drawing.Size(46, 13);
+            this.lblLiaisonTraversee.TabIndex = 7;
+            this.lblLiaisonTraversee.Text = "Liaison :";
+            // 
+            // lblBateauTraversee
+            // 
+            this.lblBateauTraversee.AutoSize = true;
+            this.lblBateauTraversee.Location = new System.Drawing.Point(403, 53);
+            this.lblBateauTraversee.Name = "lblBateauTraversee";
+            this.lblBateauTraversee.Size = new System.Drawing.Size(71, 13);
+            this.lblBateauTraversee.TabIndex = 8;
+            this.lblBateauTraversee.Text = "Nom bateau :";
+            // 
+            // lblDateDepart
+            // 
+            this.lblDateDepart.AutoSize = true;
+            this.lblDateDepart.Location = new System.Drawing.Point(347, 240);
+            this.lblDateDepart.Name = "lblDateDepart";
+            this.lblDateDepart.Size = new System.Drawing.Size(111, 13);
+            this.lblDateDepart.TabIndex = 9;
+            this.lblDateDepart.Text = "Date et heure départ :";
+            // 
+            // lblDateArrivee
+            // 
+            this.lblDateArrivee.AutoSize = true;
+            this.lblDateArrivee.Location = new System.Drawing.Point(341, 292);
+            this.lblDateArrivee.Name = "lblDateArrivee";
+            this.lblDateArrivee.Size = new System.Drawing.Size(113, 13);
+            this.lblDateArrivee.TabIndex = 10;
+            this.lblDateArrivee.Text = "Date et heure arrivée :";
+            // 
             // FormAjouterTraversee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDateArrivee);
+            this.Controls.Add(this.lblDateDepart);
+            this.Controls.Add(this.lblBateauTraversee);
+            this.Controls.Add(this.lblLiaisonTraversee);
+            this.Controls.Add(this.lblSecteursTraversee);
+            this.Controls.Add(this.btnAjouterTraversee);
+            this.Controls.Add(this.cmbBateauTraversee);
+            this.Controls.Add(this.cmbLiaisonsTraversee);
+            this.Controls.Add(this.lbxSecteursTraversee);
+            this.Controls.Add(this.dtpArrivee);
             this.Controls.Add(this.dtpDepart);
             this.Name = "FormAjouterTraversee";
             this.Text = "Ajouter une traversée";
+            this.Load += new System.EventHandler(this.FormAjouterTraversee_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DateTimePicker dtpDepart;
+        private System.Windows.Forms.DateTimePicker dtpArrivee;
+        private System.Windows.Forms.ListBox lbxSecteursTraversee;
+        private System.Windows.Forms.ComboBox cmbLiaisonsTraversee;
+        private System.Windows.Forms.ComboBox cmbBateauTraversee;
+        private System.Windows.Forms.Button btnAjouterTraversee;
+        private System.Windows.Forms.Label lblSecteursTraversee;
+        private System.Windows.Forms.Label lblLiaisonTraversee;
+        private System.Windows.Forms.Label lblBateauTraversee;
+        private System.Windows.Forms.Label lblDateDepart;
+        private System.Windows.Forms.Label lblDateArrivee;
     }
 }
--- Projet_Atlantik/FormAjouterTraversee.cs
+++ Projet_Atlantik/FormAjouterTraversee.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Ocsp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,151 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Org.BouncyCastle.Crypto.Engines.SM2Engine;
 
 namespace Projet_Atlantik
 {
     public partial class FormAjouterTraversee : Form
     {
+        MySqlConnection maCnx = new MySqlConnection("server=localhost;user=root;database=atlantik;port=3306;password=");
+        MySqlDataReader jeuEnr = null;
+
         public FormAjouterTraversee()
         {
             InitializeComponent();
         }
+
+        private void FormAjouterTraversee_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string requete;
+                maCnx.Open();
+
+
+                requete = "Select * from SECTEUR";
+                var maCde = new MySqlCommand(requete, maCnx);
+
+                // POUR SOUCIS DE TYPAGE voir exemple ExecuteNonQuery, ci-dessus
+                // FIN requête paramétrée
+
+                jeuEnr = maCde.ExecuteReader();
+                while (jeuEnr.Read())
+                {
+                    //cmbNomSecteur.Items.Add(new Secteur((int)jeuEnr["nosecteur"], (string)jeuEnr["nom"]));
+                    lbxSecteursTraversee.Items.Add(new Secteur(jeuEnr.GetInt32("nosecteur"), jeuEnr.GetString("nom")));
+                }
+                jeuEnr.Close();
+
+                requete = "Select * from BATEAU";
+                maCde = new MySqlCommand(requete, maCnx);
+
+                // POUR SOUCIS DE TYPAGE voir exemple ExecuteNonQuery, ci-dessus
+                // FIN requête paramétrée
+
+                jeuEnr = maCde.ExecuteReader();
+                while (jeuEnr.Read())
+                {
+                    //cmbNomSecteur.Items.Add(new Secteur((int)jeuEnr["nosecteur"], (string)jeuEnr["nom"]));
+                    cmbBateauTraversee.Items.Add(new Bateau(jeuEnr.GetInt32("nobateau"), jeuEnr.GetString("nom")));
+                }
+                jeuEnr.Close();
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur " + erreur.ToString());
+            }
+            finally
+            {
+                if (jeuEnr is object & !jeuEnr.IsClosed)
+                {
+                    jeuEnr.Close(); // s'il existe et n'est pas déjà fermé
+                }
+
+                if (maCnx is object & maCnx.State == ConnectionState.Open)
+                {
+                    maCnx.Close(); // on se déconnecte
+                }
+            }
+        }
+
+        private void lbxSecteursTraversee_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string requete;
+                maCnx.Open();
+                // lbxSecteursAjouterTarifs.SelectedIndex = 0;
+                requete = "SELECT liaison.NOLIAISON noliaison, liaison.NOPORT_DEPART portdepart, liaison.NOPORT_ARRIVEE portarrivee, d.NOM depart, a.NOM arrivee FROM liaison INNER JOIN secteur ON liaison.NOSECTEUR = secteur.NOSECTEUR INNER JOIN port d ON liaison.NOPORT_DEPART = d.NOPORT INNER JOIN port a ON liaison.NOPORT_ARRIVEE = a.NOPORT WHERE secteur.NOSECTEUR = @NOSECTEUR";
+                var maCde = new MySqlCommand(requete, maCnx);
+                int numero = ((Secteur)(lbxSecteursTraversee.SelectedItem)).GetNoSecteur();
+                maCde.Parameters.AddWithValue("@NOSECTEUR", numero);
+
+                // POUR SOUCIS DE TYPAGE voir exemple ExecuteNonQuery, ci-dessus
+                // FIN requête paramétrée
+
+                jeuEnr = maCde.ExecuteReader();
+                while (jeuEnr.Read())
+                {
+                    //cmbNomSecteur.Items.Add(new Secteur((int)jeuEnr["nosecteur"], (string)jeuEnr["nom"]));
+                    cmbLiaisonsTraversee.Items.Add(new Liaison(jeuEnr.GetInt32("noliaison"), jeuEnr.GetString("depart"), jeuEnr.GetString("arrivee")));
+                }
+                jeuEnr.Close();
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur " + erreur.ToString());
+            }
+            finally
+            {
+                if (jeuEnr is object & !jeuEnr.IsClosed)
+                {
+                    jeuEnr.Close(); // s'il existe et n'est pas déjà fermé
+                }
+
+                if (maCnx is object & maCnx.State == ConnectionState.Open)
+                {
+                    maCnx.Close();
+                }
+            }
+        }
+
+        private void btnAjouterTraversee_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string requete;
+                maCnx.Open(); // on se connecte
+
+                // DEBUT requête paramétrée
+                requete = "Insert into TRAVERSEE(noliaison, nobateau, dateheuredepart, dateheurearrivee, clotureembarquement) values (@NOLIAISON, @NOBATEAU, @DATEDEPART, @DATEARRIVEE, @CLOTURE)";
+                var maCde = new MySqlCommand(requete, maCnx);
+                maCde.Parameters.AddWithValue("@NOLIAISON", ((Liaison)(cmbLiaisonsTraversee.SelectedItem)).GetNoLiaison());
+                maCde.Parameters.AddWithValue("@NOBATEAU", ((Bateau)(cmbBateauTraversee.SelectedItem)).GetNoBateau());
+                maCde.Parameters.AddWithValue("@DATEDEPART", dtpDepart.Value);
+                maCde.Parameters.AddWithValue("@DATEARRIVEE", dtpArrivee.Value);
+                maCde.Parameters.AddWithValue("@CLOTURE", 0);
+                // POUR SOUCIS DE TYPAGE voir exemple ExecuteNonQuery, ci-dessus
+                // FIN requête paramétrée
+
+                int nbLigneAffectees;
+                nbLigneAffectees = maCde.ExecuteNonQuery();
+                MessageBox.Show("Nombre de ligne affectée(s) :" + nbLigneAffectees.ToString());
+
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur " + erreur.ToString());
+            }
+            finally
+            {
+                if (maCnx is object & maCnx.State == ConnectionState.Open)
+                {
+                    maCnx.Close(); // on se déconnecte
+                }
+            }
+        }
     }
-}
+    }
+
+
